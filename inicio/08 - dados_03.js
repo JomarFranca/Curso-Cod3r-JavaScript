eb9b130e3f8c2d2a0a8d2f2d952bbdb803da01af
Jomar A. França <jomar.almeida@gmail.com>
2022-10-03 20:55:19 -0300

    adicionei a aula 08

---
+++ inicio/08 - dados_03.js
@@ -0,0 +1,13 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.38;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+let nome = "Jomar";
+console.log(typeof nome);
+console.log(typeof "Jomar");
