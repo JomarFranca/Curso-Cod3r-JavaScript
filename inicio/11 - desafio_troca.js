a65cc6c5e7dfd44041c4d25efe74cbde9d25552e
Jomar A. França <jomar.almeida@gmail.com>
2022-10-03 21:39:02 -0300

    Adicionei aula 11

---
+++ inicio/11 - desafio_troca.js
@@ -0,0 +1,20 @@
+let a = 7;
+let b = 94;
+
+console.log("O primeiro valor da Letra A é: " + a);
+console.log("O primeiro valor da Letra B é: " + b);
+
+// depois da troca .... a  passa a valer 94, e b passa a valer 7.
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("Agora o valor da Letra A é: " + a);
+console.log("Agora o valor da Letra B é: " + b);
+
+//Segunda forma de fazer
+
+[a, b] = [b, a];
+console.log(a);
+console.log(b);
