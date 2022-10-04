43d053f4adff34b6b74514898b87051eaaf0f409
Jomar A. França <jomar.almeida@gmail.com>
2022-10-03 21:15:24 -0300

    Adicionei aula 10

---
+++ inicio/10 - desafio_circuferência.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+let bola = PI * raio * raio;
+
+console.log("A Terra tem o tamanho de " + bola + "m2");
+
+bola = Math.PI * raio * raio;
+console.log("A Terra tem o tamanho de " + bola + "m2");
