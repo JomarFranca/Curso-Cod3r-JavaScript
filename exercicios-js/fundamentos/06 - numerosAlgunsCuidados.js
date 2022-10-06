47c8e3d6af68676338fb21b43eaee9b1658473aa
Jomar A. França <jomar.almeida@gmail.com>
2022-10-06 19:45:17 -0300

    Adicionei aula 06 parte 02

---
+++ exercicios-js/fundamentos/06 - numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0); // qualquer número dividido por 10 geraria número infinito
+console.log("10" / 2); // fazendo uma operação com uma string e um número
+console.log("show!" * 2); //
+console.log(0.1 + 0.7); // gera uma imprecisão
+//console.log(10.toString());
+console.log((10).toFixed(2)); // não é possível chamar um valor com uma função sem coloca-lo dentro de um parênteses
+console.log((10.345).toFixed(2));
