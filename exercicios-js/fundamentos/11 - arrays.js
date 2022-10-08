6964a6aa2f3adfc2334ecc5983337aca0abce969
Jomar A. França <jomar.almeida@gmail.com>
2022-10-07 23:39:29 -0300

    Adicionei aula 12 parte 02

--- exercicios-js/fundamentos/11 - arrays.js
+++ exercicios-js/fundamentos/11 - arrays.js
@@ -1,14 +1,14 @@
 const valores = [7.7, 8.9, 6.3, 9.2];
-console.log(valores[0], valores[3]);
+console.log(valores[0], valores[3]); // usa o identificador do objeto
 console.log(valores[4]);
 
 valores[4] = 10;
 console.log(valores);
-console.log(valores.length); // seta o tamanho
-valores.push({ id: 3 }, false, null, "teste"); // aumenta o tamanho
+console.log(valores.length); // método para setar o tamanho
+valores.push({ id: 3 }, false, null, "teste"); // método p/ aumenta o tamanho
 console.log(valores);
 
-console.log(valores.pop()); // retorna o ultimo valor
+console.log(valores.pop()); // método retorna o ultimo valor
 delete valores[0];
 console.log(valores);
 
