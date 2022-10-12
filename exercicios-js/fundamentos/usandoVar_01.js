e4f7662c09d058d6e490a58150dc0d5c72711465
Jomar A. França <jomar.almeida@gmail.com>
2022-10-11 23:34:40 -0300

    Aula 17 parte 02

---
+++ exercicios-js/fundamentos/usandoVar_01.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???';
+                console.log(sera);
+            }
+        }
+    }
+}
+console.log(sera);
+
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+console.log(local); // está errado por que o var aqui foi criado do escopo de função
