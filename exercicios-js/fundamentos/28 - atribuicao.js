593b73a9d654417efe58390251a8921ea110ff5f
Jomar A. França <jomar.almeida@gmail.com>
2022-10-24 20:45:07 -0300

    Adicionei aula 28 parte 02

---
+++ exercicios-js/fundamentos/28 - atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7;
+let b = 3;
+
+b += a; // b = b + a = adição
+console.log(b);
+
+b -= 4; // b = b - 4 = subtração
+console.log(b);
+
+b *= 2; // b = b * 2 = multiplicação
+console.log(b);
+
+b /= 2; // b = b / 2 = divisão
+console.log(b);
+
+b %= 2; // b = b % 2 = resto do módulo(0 ou 1)
+console.log(b);
