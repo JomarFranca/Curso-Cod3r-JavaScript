3d6bb7724e54b078c6c3f75948899b0e741d9608
Jomar A. França <jomar.almeida@gmail.com>
2022-11-04 00:43:58 -0300

    Aula 56 parte 04 adicionado

---
+++ exercicios-js/03 - funcao/56 - thisEBind_02.js
@@ -0,0 +1,30 @@
+// Modelo 01
+function Pessoa() {
+    this.idade = 0;
+
+    setInterval(
+        function () {
+            this.idade++;
+            console.log(this.idade);
+        }.bind(this),
+        1000
+    );
+}
+
+new Pessoa();
+
+// Modelo 02
+function Pessoa() {
+    this.idade = 0;
+
+    const self = this;
+    setInterval(
+        function () {
+            self.idade++;
+            console.log(self.idade);
+        } /*.bind(self)*/,
+        1000
+    );
+}
+
+new Pessoa();
