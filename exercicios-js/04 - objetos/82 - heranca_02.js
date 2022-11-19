c0bb4b4c0e37e7076f30908a6428e37c511df2d1
Jomar A. França <jomar.almeida@gmail.com>
2022-11-19 00:25:44 -0300

    Aula 82 parte 05 - adicionado

---
+++ exercicios-js/04 - objetos/82 - heranca_02.js
@@ -0,0 +1,46 @@
+// Cadeia de Protótipos (prototype chain)
+
+Object.prototype.atrib0 = 'Z'; // NÃO USE ESTE MODELO
+const avo = { atrib1: 'A' };
+const pai = { __proto__: avo, atrib2: 'B', atrib3: '3' }; // sobrescreve a atribuição abaixo, ou seja busca no objeto mais especifico
+const filho = { __proto__: pai, atrib3: 'C' };
+console.log(filho.atrib0, filho.atrib1, filho.atrib2, filho.atrib3);
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelerarMais(delta) {
+        if (this.velAtual * delta <= this.velMax) {
+            this.velAtual += delta;
+        } else {
+            this.velAtual = this.velMax;
+        }
+    },
+    status() {
+        return `${this.velAtual}Km/h de ${this.velMax}Km/h`;
+    },
+};
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324, // shadowing
+};
+
+const volvo = {
+    modelo: 'V40',
+    status() {
+        return `${this.modelo}: ${super.status()}`;
+    },
+};
+
+Object.setPrototypeOf(ferrari, carro);
+Object.setPrototypeOf(volvo, carro);
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.acelerarMais(100);
+console.log(volvo.status());
+
+ferrari.acelerarMais(300);
+console.log(ferrari.status());
